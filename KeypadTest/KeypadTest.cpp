ed37e318da7b6f7bd909ff1644c3f9be33502bad
RoyerAriel <RoyerAriel@DESKTOP-JH0GV77>
2016-11-29 06:24:25 -0400

    test

--- KeypadTest/KeypadTest.cpp
+++ KeypadTest/KeypadTest.cpp
@@ -1,38 +1,25 @@
 // Do not remove the include below
 #include "KeyPadTest.h"
+#include "TM1637Display.h"
+#define TEST_DELAY 500
+TM1637Display display(3, 2);
 
-#include "Keypad.h"
-#include "LiquidCrystal_I2C.h"
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
-//define the cymbols on the buttons of the keypads
-char hexaKeys[ROWS][COLS] = { { '1', '2', '3', 'A' }, { '4', '5', '6', 'B' },
-    { '7', '8', '9', 'C' }, { '*', '0', '#', 'D' } };
-
-byte rowPins[ROWS] = { 22, 24, 26, 28 }; //connect to the row pinouts of the keypad
-byte colPins[COLS] = { 30, 32, 34, 36 }; //connect to the column pinouts of the keypad
-
-//initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup()
 {
-  lcd.init();
-  lcd.init();
-  Serial.begin(115200);
+  pinMode(2, INPUT);
+  digitalWrite(2, HIGH);
+
+//  uint8_t data2[] =
+//        { display.encodeDigit(3), display.encodeDigit(4), display.encodeDigit(5),
+//            display.encodeDigit(9) };
+//  display.setSegments(data2);
 }
 
 void loop()
 {
 
-  char customKey = customKeypad.getKey();
-
-  if (customKey)
-  {
-    lcd.backlight();
-    lcd.setCursor(0, 0);
-    lcd.print("keyPressed:");
-    lcd.print(customKey);
-    Serial.println(customKey);
-  }
+  pinMode(2, OUTPUT);
+  delay(10);
+  pinMode(2, INPUT);
+  delay(10);
 }
--- KeypadTest/Release/KeypadTest.cpp.d
+++ KeypadTest/Release/KeypadTest.cpp.d
@@ -10,12 +10,7 @@ KeypadTest.cpp.o: ../KeypadTest.cpp ../KeyPadTest.h \
  C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/USBAPI.h \
  C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/Arduino.h \
  C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\variants\mega/pins_arduino.h \
- C:\Users\RoyerAriel\Documents\Arduino\libraries\Keypad/Keypad.h \
- C:\Users\RoyerAriel\Documents\Arduino\libraries\Keypad/Key.h \
- C:\Users\RoyerAriel\Documents\Arduino\libraries\LiquidCrystal_I2C/LiquidCrystal_I2C.h \
- C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/Print.h \
- C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\libraries\Wire\src/Wire.h \
- C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/Stream.h
+ C:\Users\Royel\Documents\Arduino\libraries\TM1637/TM1637Display.h
 
 ../KeyPadTest.h:
 
@@ -41,14 +36,4 @@ C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino
 
 C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\variants\mega/pins_arduino.h:
 
-C:\Users\RoyerAriel\Documents\Arduino\libraries\Keypad/Keypad.h:
-
-C:\Users\RoyerAriel\Documents\Arduino\libraries\Keypad/Key.h:
-
-C:\Users\RoyerAriel\Documents\Arduino\libraries\LiquidCrystal_I2C/LiquidCrystal_I2C.h:
-
-C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/Print.h:
-
-C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\libraries\Wire\src/Wire.h:
-
-C:\Dev\Ceclipse\arduinoPlugin\packages\arduino\hardware\avr\1.6.14\cores\arduino/Stream.h:
+C:\Users\Royel\Documents\Arduino\libraries\TM1637/TM1637Display.h:
